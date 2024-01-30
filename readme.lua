bc334f7d88d4e8fdcc01a437a7666e7ac4e43211
KeoCloud <139084236+OwenDec@users.noreply.github.com>
2024-01-30 13:38:52 +0100

    LoadingScreen video ytb
    Le son est ajustable dans l'écran de charment directement, comparer à ce loadingscreen de base qui n'a pas cette fonctionnalité

---
+++ readme.lua
@@ -0,0 +1,7 @@
+https://mega.nz/file/AyUz1KCb#Bmfrs-gXBNBG1ANXA7cOeYn6kE4sE164pqbSIiEz_UM
+
+fichier du loadingscreen
+
+pas de mp
+
+merci de ne pas le partager et de ne pas le leak sous peine de suppression du fichier
