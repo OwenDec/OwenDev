95e4872cc644e73768bb3ffa45bb35af1fa85e70
KeoCloud <139084236+OwenDec@users.noreply.github.com>
2024-01-30 14:29:56 +0100

    Update readme.lua

--- readme.lua
+++ readme.lua
@@ -1,7 +1,1 @@
-https://mega.nz/file/AyUz1KCb#Bmfrs-gXBNBG1ANXA7cOeYn6kE4sE164pqbSIiEz_UM
-
-fichier du loadingscreen
-
-pas de mp
-
-merci de ne pas le partager et de ne pas le leak sous peine de suppression du fichier
+https://www.youtube.com/watch?v=j44UpOTNeUo
